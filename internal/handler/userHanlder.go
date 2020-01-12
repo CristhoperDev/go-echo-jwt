92a28b6c92ee7336ef35ae38f349dd9ce876a30f
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-01-11 19:46:04 -0500

    userHandler -> Login without jwt

--- internal/handler/userHanlder.go
+++ internal/handler/userHanlder.go
@@ -31,7 +31,36 @@ func RegisterUser(c echo.Context) error {
 	} else {
 		jsonObj.Status = http.StatusOK
 		jsonObj.Data = "User Created"
-		status = http.StatusCreated
+		status = http.StatusOK
+	}
+
+	return c.JSON(status, jsonObj)
+}
+
+func UserLogin(c echo.Context) error {
+	var jsonObj model.JsonResult
+	var status int
+
+	decoder := json.NewDecoder(c.Request().Body)
+	var userBody model.UserBody
+	if err := decoder.Decode(&userBody); err != nil {
+		jsonObj.Status = http.StatusBadRequest
+		jsonObj.Data = "Body not well formed"
+		status = http.StatusBadRequest
+		return c.JSON(status, jsonObj)
+	}
+
+	data, err := dao.UserLoginByUsernamePassword(userBody.Username, userBody.Password)
+
+
+	if len(data) == 0 {
+		jsonObj.Status = http.StatusNotFound
+		jsonObj.Data = err
+		status = http.StatusNotFound
+	} else {
+		jsonObj.Status = http.StatusOK
+		jsonObj.Data = data
+		status = http.StatusOK
 	}
 
 	return c.JSON(status, jsonObj)
