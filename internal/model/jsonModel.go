138be0f553ebec1a99860516d809d4e3d09c18f1
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-01-11 20:55:15 -0500

    add login respone for token

--- internal/model/jsonModel.go
+++ internal/model/jsonModel.go
@@ -4,7 +4,14 @@ type JsonResult struct {
 	Status int 			`json:"status"`
 	Data   interface{} 	`json:"data"`
 }
+
 type JsonResultError struct {
 	Success bool        `json:"success"`
 	Error   interface{} `json:"error"`
+}
+
+type JsonResultLogin struct {
+	Status 	int 			`json:"status"`
+	Token 	string 			`json:"token"`
+	Data 	interface{} 	`json:"data"`
 }
