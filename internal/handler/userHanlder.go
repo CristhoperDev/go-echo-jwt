2acf745955ef49705b60fca46c89ab4a3bf3b85b
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-01-11 19:14:19 -0500

    register user

---
+++ internal/handler/userHanlder.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/labstack/echo"
+	"go-echo-jwt/internal/dao"
+	"go-echo-jwt/internal/model"
+	"net/http"
+)
+
+func RegisterUser(c echo.Context) error {
+	var jsonObj model.JsonResult
+	var status int
+
+	decoder := json.NewDecoder(c.Request().Body)
+	var userBody model.UserBody
+	if err := decoder.Decode(&userBody); err != nil {
+		jsonObj.Status = http.StatusBadRequest
+		jsonObj.Data = "Body not well formed"
+		status = http.StatusBadRequest
+	}
+
+	err := dao.InsertUser(userBody.Username, userBody.Password)
+
+
+	if err != nil {
+		jsonObj.Status = http.StatusBadRequest
+		jsonObj.Data = err
+		status = http.StatusBadRequest
+	} else {
+		jsonObj.Status = http.StatusOK
+		jsonObj.Data = "User Created"
+		status = http.StatusCreated
+	}
+
+	return c.JSON(status, jsonObj)
+}
