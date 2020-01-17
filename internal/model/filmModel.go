b00b00fea919b2f82f3b13765a9bcda70ca7bdd8
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-01-17 18:06:01 -0500

    filmModel add FilModelPut

--- internal/model/filmModel.go
+++ internal/model/filmModel.go
@@ -4,3 +4,9 @@ type FilmModelPost struct {
 	Title 			string 		`json:"title"`
 	Description 	string 		`json:"description"`
 }
+
+type FilmModelPut struct {
+	IdFilm			int 		`json:"id_film"`
+	Title 			string 		`json:"title"`
+	Description 	string 		`json:"description"`
+}
