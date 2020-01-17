bae274b344e3c7c3088a73de586b4d2bd8ff6f89
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-01-17 18:06:59 -0500

    filmHandler add Film Put

--- internal/handler/filmHandler.go
+++ internal/handler/filmHandler.go
@@ -36,3 +36,31 @@ func FilmPost(c echo.Context) error {
 
 	return c.JSON(status, jsonObj)
 }
+func FilmPut(c echo.Context) error {
+	var jsonObj model.JsonResult
+	var status int
+
+	decoder := json.NewDecoder(c.Request().Body)
+	var FilmModelPut model.FilmModelPut
+	if err := decoder.Decode(&FilmModelPut); err != nil {
+		jsonObj.Status = http.StatusBadRequest
+		jsonObj.Data = "Body not well formed"
+		status = http.StatusBadRequest
+		return c.JSON(status, jsonObj)
+	}
+
+	err := dao.UpdateFilm(FilmModelPut.Title, FilmModelPut.Description, FilmModelPut.IdFilm)
+
+
+	if err != nil {
+		jsonObj.Status = http.StatusBadRequest
+		jsonObj.Data = err
+		status = http.StatusBadRequest
+	} else {
+		jsonObj.Status = http.StatusOK
+		jsonObj.Data = "Film Updated"
+		status = http.StatusOK
+	}
+
+	return c.JSON(status, jsonObj)
+}
