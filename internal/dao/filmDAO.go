339acd2d0239e5a76cfc2b80756ccac5fe92207c
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-01-17 18:28:16 -0500

    filmDAO add Delete Film

--- internal/dao/filmDAO.go
+++ internal/dao/filmDAO.go
@@ -16,9 +16,20 @@ func InsertFilm(title string, description string) error {
 	return nil
 }
 
-func UpdateFilm(title string, description string, id_film int) error {
+func UpdateFilm(title string, description string, filmId int) error {
 	sql := "UPDATE film SET title = ?, description = ? WHERE idfilm = ?"
-	rows, err := conn.DbConn.Query(sql, title, description, id_film)
+	rows, err := conn.DbConn.Query(sql, title, description, filmId)
+	defer rows.Close()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func DeleteFilm(filmId int) error {
+	sql := "DELETE FROM film WHERE idfilm = ?"
+	rows, err := conn.DbConn.Query(sql, filmId)
 	defer rows.Close()
 	if err != nil {
 		fmt.Println(err)
