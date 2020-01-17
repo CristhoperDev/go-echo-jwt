b5040607de2b14e8b4cd45c3cf01a76fe860443e
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-01-17 17:56:38 -0500

    filmHandler add FilmPost

---
+++ internal/handler/filmHandler.go
@@ -0,0 +1,38 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/labstack/echo"
+	"go-echo-jwt/internal/dao"
+	"go-echo-jwt/internal/model"
+	"net/http"
+)
+
+func FilmPost(c echo.Context) error {
+	var jsonObj model.JsonResult
+	var status int
+
+	decoder := json.NewDecoder(c.Request().Body)
+	var FilmModelPost model.FilmModelPost
+	if err := decoder.Decode(&FilmModelPost); err != nil {
+		jsonObj.Status = http.StatusBadRequest
+		jsonObj.Data = "Body not well formed"
+		status = http.StatusBadRequest
+		return c.JSON(status, jsonObj)
+	}
+
+	err := dao.InsertFilm(FilmModelPost.Title, FilmModelPost.Description)
+
+
+	if err != nil {
+		jsonObj.Status = http.StatusBadRequest
+		jsonObj.Data = err
+		status = http.StatusBadRequest
+	} else {
+		jsonObj.Status = http.StatusOK
+		jsonObj.Data = "Film Created"
+		status = http.StatusOK
+	}
+
+	return c.JSON(status, jsonObj)
+}
